4df4c3e5bb09d14bc47d3c59b275f5cb6dc4816d
adfinem <zenlaws02@gmail.com>
2025-02-28 22:49:03 +0800

    feat: learned how to create a custom card with a Vocation class parameter

--- flutter_rpg/lib/screens/create/create.dart
+++ flutter_rpg/lib/screens/create/create.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/vocation.dart';
+import 'package:flutter_rpg/screens/create/vocation_card.dart';
 import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
@@ -49,53 +51,79 @@ class _CreateState extends State<Create> {
       body: SafeArea(
         child: Container(
           padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-          child: Column(
-            children: [
-              // welcome message
-              Center(
-                child: Icon(
-                  Icons.code,
-                  color: AppColors.primaryColor,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // welcome message
+                Center(
+                  child: Icon(
+                    Icons.code,
+                    color: AppColors.primaryColor,
+                  ),
                 ),
-              ),
-              const Center(
-                child: StyledHeading("Welcome, new player."),
-              ),
-              const Center(
-                child: StyledText("Create a name & slogan for your character."),
-              ),
-              SizedBox(
-                height: 20,
-              ),
+                const Center(
+                  child: StyledHeading("Welcome, new player."),
+                ),
+                const Center(
+                  child:
+                      StyledText("Create a name & slogan for your character."),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
 
-              // inputs
-              TextField(
-                controller: _nameController,
-                style: GoogleFonts.kanit(
-                    textStyle: Theme.of(context).textTheme.bodyMedium),
-                cursorColor: AppColors.textColor,
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.person),
-                    label: StyledText("Character name")),
-              ),
-              const SizedBox(height: 30),
-              TextField(
-                controller: _sloganController,
-                style: GoogleFonts.kanit(
-                    textStyle: Theme.of(context).textTheme.bodyMedium),
-                cursorColor: AppColors.textColor,
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.chat),
-                    label: StyledText("Character slogan")),
-              ),
-              const SizedBox(height: 30),
+                // inputs
+                TextField(
+                  controller: _nameController,
+                  style: GoogleFonts.kanit(
+                      textStyle: Theme.of(context).textTheme.bodyMedium),
+                  cursorColor: AppColors.textColor,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.person),
+                      label: StyledText("Character name")),
+                ),
+                const SizedBox(height: 30),
+                TextField(
+                  controller: _sloganController,
+                  style: GoogleFonts.kanit(
+                      textStyle: Theme.of(context).textTheme.bodyMedium),
+                  cursorColor: AppColors.textColor,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.chat),
+                      label: StyledText("Character slogan")),
+                ),
+                const SizedBox(height: 30),
 
-              Center(
-                child: StyledButton(
-                    onPressed: handleSubmit,
-                    child: StyledHeading("Create Character")),
-              )
-            ],
+                // select vocation title
+                Center(
+                  child: Icon(
+                    Icons.code,
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+                const Center(
+                  child: StyledHeading("Choose a vocation."),
+                ),
+                const Center(
+                  child: StyledText("This determines your available skills."),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+
+                // vocations
+                const VocationCard(vocation: Vocation.junkie),
+                const VocationCard(vocation: Vocation.ninja),
+                const VocationCard(vocation: Vocation.raider),
+                const VocationCard(vocation: Vocation.wizard),
+
+                Center(
+                  child: StyledButton(
+                      onPressed: handleSubmit,
+                      child: StyledHeading("Create Character")),
+                )
+              ],
+            ),
           ),
         ),
       ),
